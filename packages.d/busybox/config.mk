8bd75e262135b640f81111981421cd7fdce84684
Jakob Blomer <jblomer@cern.ch>
2014-02-04 09:17:45 +0100

    fix urls

--- packages.d/busybox/config.mk
+++ packages.d/busybox/config.mk
@@ -1,1 +1,1 @@
-BB_SOURCE = http://ecsft.cern.ch/dist/cernvm/busybox-$(BB_UPSTREAM_VERSION)/busybox-$(BB_STRONG_VERSION)
+BB_SOURCE = https://ecsft.cern.ch/dist/cernvm/busybox-$(BB_UPSTREAM_VERSION)/busybox-$(BB_STRONG_VERSION)
