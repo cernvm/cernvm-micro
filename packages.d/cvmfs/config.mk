45d44e19fafcc4706e2141276a814a52995f86bf
Jakob Blomer <jblomer@cern.ch>
2015-02-04 12:10:24 +0100

    add CVMFS_HIDE_MAGIC_XATTRS=yes, cvmfs 2.1.20

--- packages.d/cvmfs/config.mk
+++ packages.d/cvmfs/config.mk
@@ -1,6 +1,6 @@
-CVMFS_SOURCETREE = cvmfs-$(CVMFS_UPSTREAM_VERSION)
-#CVMFS_SOURCETREE = cvmfs-git-1d3857a0f3cd73fc
-CVMFS_TARBALL = cvmfs-$(CVMFS_UPSTREAM_VERSION).tar.gz
-#CVMFS_TARBALL = cvmfs-git-1d3857a0f3cd73fc.tar.gz
-CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/cvmfs-$(CVMFS_UPSTREAM_VERSION)/source.tar.gz
-#CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/nightlies/cvmfs-git-304-1d3857a0f3cd73fc/source.tar.gz
+#CVMFS_SOURCETREE = cvmfs-$(CVMFS_UPSTREAM_VERSION)
+CVMFS_SOURCETREE = cvmfs-git-76f26c54cfcb0bc4
+#CVMFS_TARBALL = cvmfs-$(CVMFS_UPSTREAM_VERSION).tar.gz
+CVMFS_TARBALL = cvmfs-git-76f26c54cfcb0bc4.tar.gz
+#CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/cvmfs-$(CVMFS_UPSTREAM_VERSION)/source.tar.gz
+CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/nightlies/cvmfs-git-503-76f26c54cfcb0bc4/source.tar.gz
