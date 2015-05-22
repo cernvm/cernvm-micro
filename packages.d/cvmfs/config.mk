fe6d58626891832f38d048eb502e26023e7580a6
Jakob Blomer <jblomer@cern.ch>
2015-05-22 15:36:17 +0200

    update cvmfs

--- packages.d/cvmfs/config.mk
+++ packages.d/cvmfs/config.mk
@@ -1,6 +1,6 @@
-CVMFS_SOURCETREE = cvmfs-$(CVMFS_UPSTREAM_VERSION)
-#CVMFS_SOURCETREE = cvmfs-git-76f26c54cfcb0bc4
-CVMFS_TARBALL = cvmfs-$(CVMFS_UPSTREAM_VERSION).tar.gz
-#CVMFS_TARBALL = cvmfs-git-76f26c54cfcb0bc4.tar.gz
+#CVMFS_SOURCETREE = cvmfs-$(CVMFS_UPSTREAM_VERSION)
+CVMFS_SOURCETREE = cvmfs-git-ef3f73267907f632
+#CVMFS_TARBALL = cvmfs-$(CVMFS_UPSTREAM_VERSION).tar.gz
+CVMFS_TARBALL = cvmfs-git-ef3f73267907f632.tar.gz
 #CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/cvmfs-$(CVMFS_UPSTREAM_VERSION)/source.tar.gz
-CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/nightlies/cvmfs-git-503-76f26c54cfcb0bc4/source.tar.gz
+CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/nightlies/cvmfs-git-540-ef3f73267907f632/source.tar.gz
