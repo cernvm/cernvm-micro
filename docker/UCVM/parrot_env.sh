a81dc8ffded7053b103f26a794238fe8b4dd9bd6
Jakob Blomer <jblomer@cern.ch>
2015-07-14 14:06:02 +0200

    update docker container, update cvmfs

--- docker/UCVM/parrot_env.sh
+++ docker/UCVM/parrot_env.sh
@@ -5,11 +5,19 @@ cernvm-testing.cern.ch:url=http://hepvm.cern.ch/cvmfs/cernvm-testing.cern.ch,pub
 cernvm-devel.cern.ch:url=http://hepvm.cern.ch/cvmfs/cernvm-devel.cern.ch,pubkey=/UCVM/keys/cernvm-devel.cern.ch.pub \
 cernvm-slc4.cern.ch:url=http://hepvm.cern.ch/cvmfs/cernvm-slc4.cern.ch,pubkey=/UCVM/keys/cernvm-slc4.cern.ch.pub \
 cernvm-slc5.cern.ch:url=http://hepvm.cern.ch/cvmfs/cernvm-slc5.cern.ch,pubkey=/UCVM/keys/cernvm-slc5.cern.ch.pub \
-cernvm-sl7.cern.ch:url=http://hepvm.cern.ch/cvmfs/cernvm-sl7.cern.ch,pubkey=/UCVM/keys/cernvm-sl7.cern.ch.pub"
-if [ -f /PARROT_CVMFS_REPO ]; then
-  export PARROT_CVMFS_REPO="${PARROT_CVMFS_REPO} $($BB cat /PARROT_CVMFS_REPO)"
+cernvm-sl7.cern.ch:url=http://hepvm.cern.ch/cvmfs/cernvm-sl7.cern.ch,pubkey=/UCVM/keys/cernvm-sl7.cern.ch.pub \
+alice.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/alice.cern.ch,pubkey=/UCVM/keys/cern.ch.pub \
+alice-ocdb.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/alice-ocdb.cern.ch,pubkey=/UCVM/keys/cern-it1.cern.ch.pub \
+atlas.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/atlas.cern.ch,pubkey=/UCVM/keys/cern.ch.pub \
+atlas-condb.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/atlas-condb.cern.ch,pubkey=/UCVM/keys/cern.ch.pub \
+cms.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/cms.cern.ch,pubkey=/UCVM/keys/cern.ch.pub \
+lhcb.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/lhcb.cern.ch,pubkey=/UCVM/keys/cern.ch.pub \
+sft.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/sft.cern.ch,pubkey=/UCVM/keys/cern.ch.pub \
+grid.cern.ch:url=http://cvmfs-stratum-one.cern.ch/cvmfs/grid.cern.ch,pubkey=/UCVM/keys/cern.ch.pub"
+if [ -f /UCVM/PARROT_CVMFS_REPO ]; then
+  export PARROT_CVMFS_REPO="${PARROT_CVMFS_REPO} $($BB cat /UCVM/PARROT_CVMFS_REPO)"
 fi
-if [ -f /PARROT_OPTIONS ]; then
-  export PARROT_OPTIONS="${PARROT_OPTIONS} $($BB cat /PARROT_OPTIONS)"
+if [ -f /UCVM/PARROT_OPTIONS ]; then
+  export PARROT_OPTIONS="${PARROT_OPTIONS} $($BB cat /UCVM/PARROT_OPTIONS)"
 fi
 export HTTP_PROXY="${HTTP_PROXY:="DIRECT;DIRECT"}"
--- packages.d/cvmfs/config.mk
+++ packages.d/cvmfs/config.mk
@@ -1,6 +1,6 @@
 #CVMFS_SOURCETREE = cvmfs-$(CVMFS_UPSTREAM_VERSION)
-CVMFS_SOURCETREE = cvmfs-git-2fc9082b3f14324c
+CVMFS_SOURCETREE = cvmfs-git-88c119b94dd81309
 #CVMFS_TARBALL = cvmfs-$(CVMFS_UPSTREAM_VERSION).tar.gz
-CVMFS_TARBALL = cvmfs-git-2fc9082b3f14324c.tar.gz
+CVMFS_TARBALL = cvmfs-git-88c119b94dd81309.tar.gz
 #CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/cvmfs-$(CVMFS_UPSTREAM_VERSION)/source.tar.gz
-CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/nightlies/cvmfs-git-106/source.tar.gz
+CVMFS_SOURCE = https://ecsft.cern.ch/dist/cvmfs/nightlies/cvmfs-git-107/source.tar.gz
