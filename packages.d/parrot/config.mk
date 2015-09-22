978faee5528caef6343dbb0c5f8c698a387f42e1
Jakob Blomer <jblomer@cern.ch>
2015-09-22 10:07:03 +0200

    docker fixes

--- packages.d/parrot/config.mk
+++ packages.d/parrot/config.mk
@@ -1,5 +1,5 @@
 PARROT_GIT = https://github.com/cooperative-computing-lab/cctools.git
-PARROT_TREEISH = c3f127367
+PARROT_TREEISH = 3de4f819ad
 PARROT_SOURCETREE = parrot-$(PARROT_TREEISH)
 
 LIBCVMFS_GIT = https://github.com/cvmfs/cvmfs.git
