6d1dee28276a4d72f39583c9d6f1e2ddeb5e9874
Jakob Blomer <jblomer@cern.ch>
2016-04-01 12:01:59 +0200

    bump upstream package version

--- packages.d/busybox/config.mk
+++ packages.d/busybox/config.mk
@@ -1,3 +1,3 @@
 BB_SOURCETREE = busybox-$(BB_UPSTREAM_VERSION)
 BB_TARBALL = busybox-$(BB_UPSTREAM_VERSION).tar.bz2
-BB_SOURCE = http://busybox.net/downloads/$(BB_TARBALL)
+BB_SOURCE = https://busybox.net/downloads/$(BB_TARBALL)
