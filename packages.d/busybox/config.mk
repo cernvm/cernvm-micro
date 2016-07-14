a0c71dc4d5da62cfdcb38029b4e1cfcd5fbab3e8
Felix Scheffler <felix.scheffler@cern.ch>
2016-07-14 17:41:43 +0200

    Adjust config and Makefile for new busybox mirror
    - successfully compiled all packages in packages.d except kexec
     On branch arm64
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   packages.d/busybox/Makefile
    	new file:   packages.d/busybox/Makefile.original
    	modified:   packages.d/busybox/config.mk
    	new file:   packages.d/busybox/config.original.mk

--- packages.d/busybox/config.mk
+++ packages.d/busybox/config.mk
@@ -1,3 +1,3 @@
 BB_SOURCETREE = busybox-$(BB_UPSTREAM_VERSION)
-BB_TARBALL = busybox-$(BB_UPSTREAM_VERSION).tar.bz2
-BB_SOURCE = https://busybox.net/downloads/$(BB_TARBALL)
+BB_TARBALL = busybox-$(BB_UPSTREAM_VERSION).tar.gz
+BB_SOURCE = https://ecsft.cern.ch/dist/cernvm/$(BB_TARBALL)
---
+++ packages.d/busybox/config.original.mk
@@ -0,0 +1,3 @@
+BB_SOURCETREE = busybox-$(BB_UPSTREAM_VERSION)
+BB_TARBALL = busybox-$(BB_UPSTREAM_VERSION).tar.bz2
+BB_SOURCE = https://busybox.net/downloads/$(BB_TARBALL)
