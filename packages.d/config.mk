74214c12712441357080d2d64273c1bef500af28
Jakob Blomer <jblomer@cern.ch>
2016-08-17 23:07:47 +0200

    fix package list

--- packages.d/config.mk
+++ packages.d/config.mk
@@ -1,5 +1,5 @@
 ifeq ($(ARCH),aarch64)
     PACKAGES = busybox curl dhclient dropbear e2fsprogs ntpclient sfdisk parted cvmfs gptfdisk extras
 else
-    PACKAGES = busybox curl dhclient dropbear e2fsprogs kexec ntpclient parrot patchelf sfdisk parted cvmfs extras
+    PACKAGES = busybox curl dhclient dropbear e2fsprogs kexec ntpclient parrot patchelf sfdisk parted cvmfs gptfdisk extras
 endif
