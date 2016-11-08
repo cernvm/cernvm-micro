7f81026bdaa4be0a4c36c9bef918f19c94163601
Jakob Blomer <jblomer@cern.ch>
2016-11-08 17:31:47 +0100

    add hvm to default formats

--- config.mk
+++ config.mk
@@ -43,10 +43,10 @@ ifeq ($(ARCH),aarch64)
 	IMAGE_FORMATS = hdd
 else
 	CERNVM_BRANCHES = prod testing devel slc4 slc5 sl7
-	IMAGE_FORMATS = fat iso hdd vdi vhd vmdk tar.gz ova box
+	IMAGE_FORMATS = fat iso hdd hvm vdi vhd vmdk tar.gz ova box
 endif
-CERNVM_BRANCHES = sl7
-IMAGE_FORMATS = ova iso
+#CERNVM_BRANCHES = sl7
+#IMAGE_FORMATS = ova iso
 
 SIGNING_SERVER = cvm-sign01.cern.ch
 SINGING_URL = https://$(SIGNING_SERVER)/cgi-bin/cernvm/sign-image
