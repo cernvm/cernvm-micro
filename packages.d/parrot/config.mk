dd63119015075f414a40a0a9c546664d57108c96
Jakob Blomer <jblomer@cern.ch>
2017-01-18 13:30:35 +0100

    update parrot

--- packages.d/parrot/config.mk
+++ packages.d/parrot/config.mk
@@ -1,5 +1,5 @@
 PARROT_GIT = https://github.com/cooperative-computing-lab/cctools.git
-PARROT_TREEISH = 5c2f35785bb4
+PARROT_TREEISH = e76290b
 PARROT_SOURCETREE = parrot-$(PARROT_TREEISH)
 
 LIBCVMFS_GIT = https://github.com/cvmfs/cvmfs.git
