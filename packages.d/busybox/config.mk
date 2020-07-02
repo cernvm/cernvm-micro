f32fb09cc8a9f359bfadae931a639b8a2557a5e2
Jakob Blomer <jblomer@cern.ch>
2020-07-02 10:19:42 +0200

    2020.07-1 release

--- packages.d/busybox/config.mk
+++ packages.d/busybox/config.mk
@@ -1,3 +1,3 @@
 BB_SOURCETREE = busybox-$(BB_UPSTREAM_VERSION)
-BB_TARBALL = busybox-$(BB_UPSTREAM_VERSION).tar.gz
+BB_TARBALL = busybox-$(BB_UPSTREAM_VERSION).tar.bz2
 BB_SOURCE = https://ecsft.cern.ch/dist/cernvm/$(BB_TARBALL)
