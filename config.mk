56f19c42b672493980122ce0f764f61fdb91a3ea
Jakob Blomer <jblomer@cern.ch>
2020-09-16 12:31:00 +0200

    add release howto

--- config.mk
+++ config.mk
@@ -44,8 +44,8 @@ else
 	CERNVM_BRANCHES = prod v3prod v4prod v3testing v4testing devel sl7 slc4 slc5
 	IMAGE_FORMATS = fat iso hdd hvm vdi vhd vmdk tar.gz ova box qcow2
 endif
-CERNVM_BRANCHES = prod v4prod v4testing sl7
-IMAGE_FORMATS = iso ova hdd
+#CERNVM_BRANCHES = v4prod
+#IMAGE_FORMATS = ova
 
 SIGNING_SERVER = cvm-sign02.cern.ch
 SIGNING_URL = https://$(SIGNING_SERVER)/cgi-bin/cernvm/sign-image
